38afb1d82816ce5e2aa8fe779c72a910d1222ef9
davnils <nilsson.dd+github+gravatarobscurity@gmail.com>
2013-12-07 01:06:38 +0100

    Add verification of nice tree decompositions.

--- src/TestTreeDecomposition.cc
+++ src/TestTreeDecomposition.cc
@@ -1,9 +1,13 @@
+#include <algorithm>
 #include <boost/graph/graphviz.hpp>
 #include <boost/graph/random.hpp>
 #include <boost/random.hpp>
 #include <dlib/graph_utils.h>
 #include <dlib/graph.h>
 #include <iostream>
+#include <list>
+#include <queue>
+#include <vector>
 
 #include "TestUtils.hpp"
 #include "TreeDecomposition.hpp"
@@ -25,6 +29,73 @@ static bool isBinaryTree(const tree_decomp_t & tree)
     if(degree < 1 || degree > 3)
     {
       return false;
+    }
+  }
+
+  return true;
+}
+
+/**
+ */
+static bool isNiceTreeDecomposition(const tree_decomp_t & tree, unsigned int root)
+{
+  //Check invariant 1
+  if(!isBinaryTree(tree))
+  {
+    return false;
+  }
+
+  //Check invariants 2 and 3 by performing a BFS traversal from the root
+  std::vector<bool> visited(boost::num_vertices(tree), false);
+  std::queue<unsigned int> work;
+
+  work.push(root);
+  visited.at(root) = true;
+
+  while(!work.empty())
+  {
+    auto vertex = work.front(); work.pop();
+    auto it = boost::adjacent_vertices(vertex, tree);
+
+    std::list<unsigned int> childs(it.first, it.second);
+    for(auto & u : childs)
+    {
+      if(visited.at(u))
+      {
+        childs.remove(u);
+        break;
+      }
+    }
+
+    //One child case
+    if(childs.size() == 1)
+    {
+      auto const & c = tree[childs.front()];
+      auto const & v = tree[vertex];
+
+      auto first  = v.size() == c.size() + 1 &&
+                    std::includes(c.begin(), c.end(), v.begin(), v.end());
+      auto second = v.size() == c.size() - 1 &&
+                    std::includes(v.begin(), v.end(), c.begin(), c.end());
+
+      if(!first && !second)
+      {
+        return false;
+      }
+      work.push(childs.front());
+    }
+    //Two children case
+    else if(childs.size() == 2)
+    {
+      auto first = childs.front(); childs.pop_front();
+      auto second = childs.front(); childs.pop_front();
+      if(tree[first] != tree[second] || tree[first] != tree[vertex])
+      {
+        return false;
+      }
+
+      work.push(first);
+      work.push(second);
     }
   }
 
