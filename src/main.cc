df7522c08129e3c51491038ea1f16143b446f227
davnils <nilsson.dd+github+gravatarobscurity@gmail.com>
2014-01-22 14:42:50 +0100

    Add basic test suite main driver.

--- src/main.cc
+++ src/main.cc
@@ -1,42 +1,38 @@
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/graphviz.hpp>
-
 #include "TestTreeDecomposition.hpp"
 #include "TreeDecomposition.hpp"
-
-static void checkDiaz()
-{
-  Tree::undirected_graph_t graph(7);
-  enum {A, B, C, D, E, F, G} edge_t;
-
-  boost::add_edge(A, B, graph);
-  boost::add_edge(A, G, graph);
-  boost::add_edge(B, C, graph);
-  boost::add_edge(B, G, graph);
-  boost::add_edge(C, D, graph);
-  boost::add_edge(C, E, graph);
-  boost::add_edge(D, E, graph);
-  boost::add_edge(E, F, graph);
-  boost::add_edge(E, G, graph);
-  boost::add_edge(F, G, graph);
-  boost::add_edge(F, C, graph);
-
-  std::vector<std::string> labels = {"a", "b", "c", "d", "e", "f", "g"};
-  boost::write_graphviz(std::cout, graph, boost::make_label_writer(&labels[0]));
-  std::cout << std::endl;
-
-  auto result = Tree::buildTreeDecomposition(graph);
-  Tree::visualizeDecomposition(std::cout, result);
-
-  auto nice = Tree::convertToNiceDecomposition(result);
-  Tree::visualizeDecomposition(std::cout, nice.first);
-  std::cout << "Root vertex: " << nice.second << std::endl;
-}
+#include "TestHomomorphism.hpp"
+#include "TestCount.hpp"
+#include "TestBasicEquation.hpp"
+#include "TestSimpleEquation.hpp"
 
 int main()
 {
-  //Tree::Test::testBinaryDecomposition(std::cout);
-  Tree::Test::testNiceTreeDecomposition(std::cout);
-  Tree::Test::testCountHomomorphisms(std::cout);
+  using namespace Count::Tree::Test;
+  testBinaryDecomposition(std::cout);
+  testNiceTreeDecomposition(std::cout);
+
+  using namespace Count::Homomorphism::Test;
+  testStingyOrdering(std::cout);
+  testNaiveCounting(std::cout);
+  testCountHomomorphisms(std::cout);
+  testSimpleHomomorphism(std::cout);
+
+  using namespace Count::Test;
+  testCountInjective(std::cout);
+  testPartitionGraph(std::cout);
+  testCountInjectiveTriplesPath(std::cout);
+  testCountInjectiveTriplesKPath(std::cout);
+  testCountInjectiveTriples(std::cout);
+
+  using namespace Count::Basic::Test;
+  testLTable(std::cout);
+  testBasicEquations(std::cout);
+
+  using namespace Count::Simple::Test;
+  testSymDiffProduct(std::cout);
+  testSimpleEquations(std::cout);
+
+  testCountIsoSubgraphs(std::cout);
+
   return 0;
 }
